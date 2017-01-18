0a18a539f31bfccbb951a4ee65567548d162b7a2
Amer Hamdan <amashamdan@gmail.com>
2017-01-17 17:35:25 -0800

    Feat: Added post request to /login.
    The request checks for the entered email and password.
    If any is incorrect login page is rendered again with an error message.
    If all checks out, session created and user redirected to dashboard page.

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,43 @@
 var express = require("express");
 var mongodb = require("mongodb");
+var parser = require("body-parser").urlencoded({extended: false});
+var bcrypt = require("bcryptjs");
 
 var router = express.Router();
+var MongoClient = mongodb.MongoClient;
+var mongoUrl = process.env.DATABASE;
 
 router.route("/")
 .get(function(req, res) {
-	res.render("login.ejs");
+	res.render("login.ejs", {csrfToken: req.csrfToken(), message: undefined});
 })
 
+.post(parser, function(req, res) {
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) {
+			res.end("Error connecting to database.");
+			return;
+		}
+
+		var users = db.collection("users");
+		users.find({"email": req.body.email}).toArray(function(err, result) {
+			if (result.length == 0) {
+				res.render("login.ejs", {csrfToken: req.csrfToken(), message: "Email not found.", messageType: "error"});
+			} else {
+				bcrypt.compare(req.body.password, result[0].password, function(err, checkResult) {
+					if (checkResult) {
+						req.session.user = result[0];
+						delete req.session.user.password;
+						res.redirect("/dashboard");
+					} else {
+						res.render("login.ejs", {csrfToken: req.csrfToken(), message: "Incorrect password.", messageType: "error"});
+					}
+				})
+			}
+		})
+	});
+});
+
 module.exports = router;
+
+/* USE CSRF */
