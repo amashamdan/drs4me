e06e6734eedaf6d6c5989289f591ae9daa0755f1
Amer Hamdan <amashamdan@gmail.com>
2017-01-17 17:36:57 -0800

    Feat: Added post request to /signup.
    The callback function checks if the entered email is already in use, if so the page is rendered again with an error message.
    If email is new, the user is added to the database and the login page is rendered with  a success message.

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,48 @@
 var express = require("express");
 var mongodb = require("mongodb");
+var parser = require("body-parser").urlencoded({extended: false});
+var bcrypt = require("bcryptjs");
 
 var router = express.Router();
+var MongoClient = mongodb.MongoClient;
+var mongoUrl = process.env.DATABASE;
 
 router.route("/")
 .get(function(req, res) {
-	res.render("signup.ejs");
+	res.render("signup.ejs", {csrfToken: req.csrfToken(), message: undefined});
 })
+.post(parser, function(req, res) {
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) {
+			res.end("Error connecting to database.");
+			return;
+		}
+
+		var users = db.collection("users");
+		users.find({"email": req.body.email}).toArray(function(err, results) {
+			if (results.length == 0) {
+				bcrypt.genSalt(10, function(err, salt) {
+					bcrypt.hash(req.body.password1, salt, function(err, hash) {
+						users.insert({
+							"email": req.body.email,
+							"password": hash,
+							"firstName": req.body.firstName,
+							"lastName": req.body.lastName,
+							"phone": req.body.phone
+						}, function() {
+							res.render("login.ejs", {csrfToken: req.csrfToken(), message: "You have successfully signed up. You can now login to your account.", messageType: "success"});
+						});						
+					});
+				});
+			} else {
+				res.render("signup.ejs", {csrfToken: req.csrfToken(), message: "Email is already used."});
+			}
+		})
+	})
+});
+
 
 module.exports = router;
+
+
+
