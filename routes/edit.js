c51b964a847cb297e05e4ff35abfc98693ab6db5
Amer Hamdan <amashamdan@gmail.com>
2017-01-21 14:38:40 -0800

    Feat: The user can change the password.
    From the edit page, once the user submits change password form, the code checks that the entered current password is correct.
    If yes, the databse is updated with the new password and the user is redirected to the dashbaord.
    If not, the edit page is rendered again with an error message.

--- routes/edit.js
+++ routes/edit.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongodb = require("mongodb");
 var parser = require("body-parser").urlencoded({extended: false});
+var bcrypt = require("bcryptjs");
 
 var router = express.Router();
 var MongoClient = mongodb.MongoClient;
@@ -11,31 +12,56 @@ router.route("/")
 	if (!req.session.user) {
 		res.redirect("/login");
 	} else {
-		res.render("edit.ejs", {user: req.session.user, csrfToken: req.csrfToken(), message: undefined});
+		res.render("edit.ejs", {user: req.session.user, csrfToken: req.csrfToken(), message: undefined, messagePassword: undefined});
 	}
 });
 
-router.route("/info")
-.post(parser, function(req, res) {
-	MongoClient.connect(mongoUrl, function(err, db) {
-		if (err) {
-			res.end("Error connecting to database.");
-			return;
-		}
-		var users = db.collection("users");
+
+MongoClient.connect(mongoUrl, function(err,db) {
+	if (err) {
+		res.end("Error connecting to database.");
+		return;
+	}
+	var users = db.collection("users");	
+
+	router.route("/info")
+	.post(parser, function(req, res) {
 		if (req.session.user.email == req.body.email) {
 			updateUser(req, res, users);
 		} else {
 			/* Ensure the newly entered email is not in the database. */
 			users.find({"email": req.body.email}).toArray(function(err, result) {
 				if (result.length > 0) {
-					res.render("edit.ejs", {user: req.session.user, csrfToken: req.csrfToken(), message: "The entered email is already registered."});
+					res.render("edit.ejs", {user: req.session.user, csrfToken: req.csrfToken(), message: "The entered email is already registered.", messagePassword: undefined});
 				} else {
 					updateUser(req, res, users);
 				}
 			});
 		}
-	})
+	});
+
+	router.route("/password")
+	.post(parser, function(req, res) {
+		users.find({"email": req.session.user.email}).toArray(function(err, result) {
+			bcrypt.compare(req.body.oldPassword, result[0].password, function(err, checkResult) {
+				if (checkResult) {
+					bcrypt.genSalt(10, function(err, salt) {
+						bcrypt.hash(req.body.newPassword1, salt, function(err, hash) {
+							users.update(
+								{"email": req.session.user.email},
+								{"$set": {"password": hash}},
+								function() {
+									res.redirect("/dashboard");
+								}
+							);
+						});
+					});
+				} else {
+					res.render("edit.ejs", {user: req.session.user, csrfToken: req.csrfToken(), message: undefined, messagePassword: "The current password is incorrect, changes not made."});
+				}
+			})
+		})
+	});
 });
 
 function updateUser(req, res, users) {
